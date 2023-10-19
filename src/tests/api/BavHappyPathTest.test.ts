5533e835a8bb99b049c59cc15e588fb5452e242e
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-10-19 14:42:46 +0100

    Fixing merge conflicts, infra tests, linting

--- src/tests/api/BavHappyPathTest.test.ts
+++ src/tests/api/BavHappyPathTest.test.ts
@@ -1,33 +1,33 @@
 import bavStubPayload from "../data/exampleStubPayload.json";
 import { authorizationGet, 
-    startStubServiceAndReturnSessionId, 
-    stubStartPost, tokenPost, 
-    userInfoPost, 
-    validateWellKnownResponse, 
-    wellKnownGet} 
-    from "../utils/ApiTestSteps";
+	startStubServiceAndReturnSessionId, 
+	stubStartPost, tokenPost, 
+	userInfoPost, 
+	validateWellKnownResponse, 
+	wellKnownGet } 
+	from "../utils/ApiTestSteps";
 
 
 describe("Test BAV End Points", ()=> {
-    let sessionId: any;
+	let sessionId: any;
 
-    beforeEach( async () =>{
-        //Session Request
-        const stubResponse = await startStubServiceAndReturnSessionId(bavStubPayload);
-        sessionId = stubResponse.data.session_id;
-    })
-    it.skip("E2E BAV End Points Happy Path Journey", async () =>{
-        // Authorization
-        expect(sessionId).toBeTruthy();
-        const authResponse = await authorizationGet(sessionId);
-        expect(authResponse.status).toBe(200);
-        // Token
-        const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
-        expect(tokenResponse).toBe(200);
-        // User Info
-        const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
-        expect(userInfoResponse).toBe(202);
-    })
+	beforeEach( async () =>{
+		//Session Request
+		const stubResponse = await startStubServiceAndReturnSessionId(bavStubPayload);
+		sessionId = stubResponse.data.session_id;
+	});
+	it.skip("E2E BAV End Points Happy Path Journey", async () =>{
+		// Authorization
+		expect(sessionId).toBeTruthy();
+		const authResponse = await authorizationGet(sessionId);
+		expect(authResponse.status).toBe(200);
+		// Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
+		expect(tokenResponse).toBe(200);
+		// User Info
+		const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
+		expect(userInfoResponse).toBe(202);
+	});
 });
 
 describe("E2E Happy Path Well Known Endpoint", () => {
@@ -38,5 +38,4 @@ describe("E2E Happy Path Well Known Endpoint", () => {
 		expect(wellKnownResponse.status).toBe(200);
 	});
 });
-
 
