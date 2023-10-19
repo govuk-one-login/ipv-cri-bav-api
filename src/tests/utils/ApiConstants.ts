66adb4cf0b4fe34ffacef2797c61e9a332ca195d
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-10-19 14:42:46 +0100

    Fixing merge conflicts, infra tests, linting

--- src/tests/utils/ApiConstants.ts
+++ src/tests/utils/ApiConstants.ts
@@ -1,6 +1,6 @@
 export const constants = {
-    DEV_IPV_BAV_STUB_URL : process.env.DEV_IPV_BAV_STUB_URL,
-    DEV_CRI_BAV_API_URL : process.env.DEV_CRI_BAV_API_URL,
+	DEV_IPV_BAV_STUB_URL : process.env.DEV_IPV_BAV_STUB_URL,
+	DEV_CRI_BAV_API_URL : process.env.DEV_CRI_BAV_API_URL,
 	DEV_BAV_TEST_HARNESS_URL: process.env.DEV_BAV_TEST_HARNESS_URL,
 	DEV_BAV_SESSION_TABLE_NAME: "session-bav-cri-ddb",
 };
