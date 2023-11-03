2e6334431ac4078ca2cb866b16bc20d5d782a2f3
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-11-03 14:50:47 +0000

    [KIWI-1399] Add test harness interceptor

--- src/tests/utils/ApiTestSteps.ts
+++ src/tests/utils/ApiTestSteps.ts
@@ -1,3 +1,4 @@
+import { fromNodeProviderChain } from "@aws-sdk/credential-providers"; 
 import axios, { AxiosInstance } from "axios";
 import { aws4Interceptor } from "aws4-axios";
 import { XMLParser } from "fast-xml-parser";
@@ -9,14 +10,24 @@ import { ISessionItem } from "../../models/ISessionItem";
 const API_INSTANCE = axios.create({ baseURL: constants.DEV_CRI_BAV_API_URL });
 const ajv = new Ajv({ strict: false });
 
-const HARNESS_API_INSTANCE: AxiosInstance = axios.create({ baseURL: constants.DEV_BAV_TEST_HARNESS_URL });
+const HARNESS_API_INSTANCE : AxiosInstance = axios.create({ baseURL: constants.DEV_BAV_TEST_HARNESS_URL });
+
+const customCredentialsProvider = {
+	getCredentials: fromNodeProviderChain({
+		timeout: 1000,
+		maxRetries: 0,
+	}),
+};
 const awsSigv4Interceptor = aws4Interceptor({
 	options: {
 		region: "eu-west-2",
 		service: "execute-api",
 	},
+	credentials: customCredentialsProvider,
 });
+
 HARNESS_API_INSTANCE.interceptors.request.use(awsSigv4Interceptor);
+
 const xmlParser = new XMLParser();
 
 export async function startStubServiceAndReturnSessionId(bavStubPayload: any): Promise<any> {
