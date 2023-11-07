5a93634bb42b3daf2adb508345f1100e57d1694a
Jack Mardell <Jack.mardell@digital.cabinet-office.gov.uk>
2023-11-07 13:31:23 +0000

    Current Changes

--- src/tests/api/BavHappyPath.test.ts
+++ src/tests/api/BavHappyPath.test.ts
@@ -1,87 +1,87 @@
 import bavStubPayload from "../data/exampleStubPayload.json";
 import { constants } from "../utils/ApiConstants";
 import {
-	authorizationGet,
-	getSessionAndVerifyKey,
-	getSessionAndVerifyKeyExists,
-	getSqsEventList,
-	startStubServiceAndReturnSessionId,
-	tokenPost,
-	validateTxMAEventData,
-	validateWellKnownResponse,
-	wellKnownGet,
+    authorizationGet,
+    getSessionAndVerifyKey,
+    getSessionAndVerifyKeyExists,
+    getSqsEventList,
+    startStubServiceAndReturnSessionId,
+    tokenPost,
+    userInfoPost,
+    validateJwtToken,
+    validateTxMAEventData,
+    validateWellKnownResponse,
+    wellKnownGet,
 }
-	from "../utils/ApiTestSteps";
+    from "../utils/ApiTestSteps";
 
 describe("Test BAV End Points", () => {
-	let sessionId: any;
+    let sessionId: any;
 
-	beforeEach(async () => {
-		//Session Request
-		sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
-	});
+    beforeEach(async () => {
+        //Session Request
+        sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
+    });
 
-	it("BAV CRI /session Happy Path", async () => {
-		expect(sessionId).toBeTruthy();
+    it("BAV CRI /session Happy Path", async () => {
+        expect(sessionId).toBeTruthy();
 
-		// Make sure authSession state is as expected
-		await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_SESSION_CREATED");
+        // Make sure authSession state is as expected
+        await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_SESSION_CREATED");
 
-		// Make sure txma event is present & valid
-		const sqsMessage = await getSqsEventList("txma/", sessionId, 1);
-		await validateTxMAEventData(sqsMessage);
-	});
+        // Make sure txma event is present & valid
+        const sqsMessage = await getSqsEventList("txma/", sessionId, 1);
+        await validateTxMAEventData(sqsMessage);
+    });
 
 
-	it.skip("BAV CRI /authorization Happy Path", async () => {
-		expect(sessionId).toBeTruthy();
+    it.skip("BAV CRI /authorization Happy Path", async () => {
+        expect(sessionId).toBeTruthy();
 
-		// Authorization
-		const authResponse = await authorizationGet(sessionId);
-		expect(authResponse.status).toBe(200);
-		expect(authResponse.data.authorizationCode).toBeTruthy();
-		expect(authResponse.data.redirect_uri).toBeTruthy();
-		expect(authResponse.data.state).toBeTruthy();
+        // Authorization
+        const authResponse = await authorizationGet(sessionId);
+        expect(authResponse.status).toBe(200);
+        expect(authResponse.data.authorizationCode).toBeTruthy();
+        expect(authResponse.data.redirect_uri).toBeTruthy();
+        expect(authResponse.data.state).toBeTruthy();
 
-		// Make sure authSession state is as expected
-		await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_AUTH_CODE_ISSUED");
+        // Make sure authSession state is as expected
+        await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_AUTH_CODE_ISSUED");
 
-		// Make sure txma event is present & valid
-		const sqsMessage = await getSqsEventList("txma/", sessionId, 2);
-		await validateTxMAEventData(sqsMessage);
-	});
+        // Make sure txma event is present & valid
+        const sqsMessage = await getSqsEventList("txma/", sessionId, 2);
+        await validateTxMAEventData(sqsMessage);
+    });
 
-	it.skip("BAV CRI /authorization - Repeated request made", async () => {
-		const origSessionId = sessionId;
-		const authResponse = await authorizationGet(sessionId);
-		const authCode = authResponse.data.authorizationCode;
-		const authRepeatResponse = await authorizationGet(origSessionId);
-		const authRepeatResponseCode = authRepeatResponse.data.authorizationCode;
-		expect(authCode).not.toEqual(authRepeatResponseCode);
-	});
+    it.skip("BAV CRI /authorization - Repeated request made", async () => {
+        const origSessionId = sessionId;
+        const authResponse = await authorizationGet(sessionId);
+        const authCode = authResponse.data.authorizationCode;
+        const authRepeatResponse = await authorizationGet(origSessionId);
+        const authRepeatResponseCode = authRepeatResponse.data.authorizationCode;
+        expect(authCode).not.toEqual(authRepeatResponseCode);
+    });
 
-	it.skip("BAV CRI /token Happy Path", async () => {
-		const authResponse = await authorizationGet(sessionId);
+    it.skip("BAV CRI /token Happy Path", async () => {
+        const authResponse = await authorizationGet(sessionId);
 
-		// Token
-		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
-		expect(tokenResponse.status).toBe(200);
+        // Token
+        const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
+        expect(tokenResponse.status).toBe(200);
 
-		// Verify access token expiry date exists
-		await getSessionAndVerifyKeyExists(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "accessTokenExpiryDate");
+        // Verify access token expiry date exists
+        await getSessionAndVerifyKeyExists(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "accessTokenExpiryDate");
 
-		// Check session state
-		await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_ACCESS_TOKEN_ISSUED");
-	});
-    it.only("BAV CRI /userinfo Happy Path", async () => {
-        // const documentSelectionResponse = await postDocumentSelection(docSelectionData, sessionId);
+        // Check session state
+        await getSessionAndVerifyKey(sessionId, constants.DEV_BAV_SESSION_TABLE_NAME, "authSessionState", "BAV_ACCESS_TOKEN_ISSUED");
+    });
+    
+    it.skip("BAV CRI /userinfo Happy Path", async () => {
+        const authResponse = await authorizationGet(sessionId);
 
-        // const authResponse = await authorizationGet(sessionId);
+        const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
 
-        // const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
-
-        // const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiIzZjUyYzczYi05OGM2LTQxZjMtOTA5Zi1hMTAwOTFjODI3YzgiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTYzMjUxMH0.hlPhPY50RL2nNJt4HU9T3j8I99leRYwpy0EtlqIrJRy-GcWFhR0DxMsWw6uH3RxioVyu9iGJOw2WNuqaOmXJyg");
+        const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
         expect(userInfoResponse.status).toBe(200);
 
         // Check to make sure VC JWT is present in the response and validate its contentss
@@ -98,10 +98,10 @@ describe("Test BAV End Points", () => {
 });
 
 describe("E2E Happy Path Well Known Endpoint", () => {
-	it("E2E Happy Path Journey - Well Known", async () => {
-		// Well Known
-		const wellKnownResponse = await wellKnownGet();
-		validateWellKnownResponse(wellKnownResponse.data);
-		expect(wellKnownResponse.status).toBe(200);
-	});
+    it("E2E Happy Path Journey - Well Known", async () => {
+        // Well Known
+        const wellKnownResponse = await wellKnownGet();
+        validateWellKnownResponse(wellKnownResponse.data);
+        expect(wellKnownResponse.status).toBe(200);
+    });
 });
--- src/tests/api/BavUnhappyPath.test.ts
+++ src/tests/api/BavUnhappyPath.test.ts
@@ -1,76 +1,89 @@
 import bavStubPayload from "../data/exampleStubPayload.json";
+import { constants } from "../utils/ApiConstants";
 import {
-	authorizationGet,
-	sessionPost,
-	startStubServiceAndReturnSessionId,
-	stubStartPost,
+    authorizationGet,
+    sessionPost,
+    startStubServiceAndReturnSessionId,
+    stubStartPost,
     userInfoPost,
-	tokenPost,
+    tokenPost,
 } from "../utils/ApiTestSteps";
 
 // eslint-disable-next-line @typescript-eslint/tslint/config
 describe.skip("/token Unhappy Path - invalid session state", () => {
-	let sessionId: any;
+    let sessionId: any;
 
-	beforeEach(async () => {
-		//Session Request
-		sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
-	});
+    beforeEach(async () => {
+        //Session Request
+        sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
+    });
 
-	it("Request to /token with invalid session state", async () => {
-		// Authorization
-		const authResponse = await authorizationGet(sessionId);
+    it("Request to /token with invalid session state", async () => {
+        // Authorization
+        const authResponse = await authorizationGet(sessionId);
 
-		// Token
-		await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
+        // Token
+        await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
 
-		// Request to /token endpoing again (which now has an invalid session state)
-		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
-		expect(tokenResponse.status).toBe(401);
-	});
+        // Request to /token endpoing again (which now has an invalid session state)
+        const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
+        expect(tokenResponse.status).toBe(401);
+    });
 });
 
 describe("/session Unhappy Path", () => {
-	let stubResponse: any;
-	beforeEach(async () => {
-		stubResponse = await stubStartPost(bavStubPayload);
-	});
+    let stubResponse: any;
+    beforeEach(async () => {
+        stubResponse = await stubStartPost(bavStubPayload);
+    });
 
-	it("E2E Unhappy Path Journey - Invalid Request", async () => {
-		const sessionResponse = await sessionPost(stubResponse.data.clientId, "");
+    it("E2E Unhappy Path Journey - Invalid Request", async () => {
+        const sessionResponse = await sessionPost(stubResponse.data.clientId, "");
 
-		expect(sessionResponse.status).toBe(401);
-		expect(sessionResponse.data).toBe("Unauthorized");
-	});
+        expect(sessionResponse.status).toBe(401);
+        expect(sessionResponse.data).toBe("Unauthorized");
+    });
 
-	it("E2E Unhappy Path Journey - Invalid ClientID", async () => {
-		const sessionResponse = await sessionPost("", stubResponse.data.request);
+    it("E2E Unhappy Path Journey - Invalid ClientID", async () => {
+        const sessionResponse = await sessionPost("", stubResponse.data.request);
 
-		expect(sessionResponse.status).toBe(400);
-		expect(sessionResponse.data).toBe("Bad Request");
-	});
+        expect(sessionResponse.status).toBe(400);
+        expect(sessionResponse.data).toBe("Bad Request");
+    });
 });
 
-describe("/userinfo Unhappy Path - ", () => {
+describe("E2E Journey - /userinfo Unhappy Path", () => {
+    let sessionId: any;
+    let tokenResponse: any;
+
+    beforeEach(async () => {
+        //Session Request
+        sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
+
+        const authResponse = await authorizationGet(sessionId);
+        tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri);
+
+        // TODO: Verify account here
+    });
+
     it.skip("E2E Unhappy Path Journey - Non-bearer type authentication", async () => {
-        // const userInfoResponse = await userInfoPost("Basic " + tokenResponse.data.access_token);
-        const userInfoResponse = await userInfoPost("Basic eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiJhNzhjYTE2Yi1mNTQyLTQzNzgtOWNkMC1kMGFhMTc1OTYxY2QiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTM3MjIwN30.aD-ANk6ITSjD4wQ8t9a9Qq8TgW5zXg2pxRYF5qPGK3kCqfWrHo9g_sU35rX8XFeST2BenBHoYkMZ-C7t4k1oiw");
+        const userInfoResponse = await userInfoPost("Basic " + tokenResponse.data.access_token);
         expect(userInfoResponse.status).toBe(401);
     });
 
     it.skip("E2E Unhappy Path Journey - Expired bearer token", async () => {
         // Hardcoded expired Bearer Token
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiJhNzhjYTE2Yi1mNTQyLTQzNzgtOWNkMC1kMGFhMTc1OTYxY2QiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTM3MjIwN30.aD-ANk6ITSjD4wQ8t9a9Qq8TgW5zXg2pxRYF5qPGK3kCqfWrHo9g_sU35rX8XFeST2BenBHoYkMZ-C7t4k1oiw");
+        const userInfoResponse = await userInfoPost("Bearer " + constants.DEV_IPV_BAV_EXPIRED_TOKEN);
         expect(userInfoResponse.status).toBe(401);
     });
-    
+
     it.skip("E2E Unhappy Path Journey - Missing subject field", async () => {
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiIwNmZmN2QxOS1iN2Y1LTRiZjctYmJhNi00NjRjY2FlZTdiMWQiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTQ2MDk3N30.-DgJu1RIgFgDMvTq5m2ICvzXBEvm_ICMkiWg8C7daqee9wCYZ9oRIgELoRIphxTkTQxsggVuIIvP2lCbJumGJA");
+        const userInfoResponse = await userInfoPost("Bearer " + constants.DEV_IPV_BAV_MISSING_SUBJECT_TOKEN);
         expect(userInfoResponse.status).toBe(400);
     });
 
     it.skip("E2E Unhappy Path Journey - Missing client session id field", async () => {
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiI0OTgyNGNlYS0yNzk0LTQyMTUtYjMwYy1hMTNiMThmM2JlN2QiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTUzNDQ1NH0.UXhKNSAEGkWuFKRaF_3ZFn5RtPnkv3XftW5Cn6Xci1HtjhALPpXDzSPgdouYNm8ExSNMouBOKJ3PjbgPVsFxpw");
+        const userInfoResponse = await userInfoPost("Bearer " + constants.DEV_IPV_BAV_MISSING_CLIENT_ID_TOKEN);
         expect(userInfoResponse.status).toBe(400);
     });
 });
