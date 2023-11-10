798faeae4619f0d0dc5507b3f4737549ef365e63
Jack Mardell <Jack.mardell@digital.cabinet-office.gov.uk>
2023-11-10 17:17:07 +0000

    Reverted some skipped tests and tidied up code

--- src/tests/api/BavUnhappyPath.test.ts
+++ src/tests/api/BavUnhappyPath.test.ts
@@ -64,16 +64,6 @@ describe("/userinfo Unhappy Path - ", () => {
         expect(userInfoResponse.status).toBe(401);
 
     });
-
-    it.skip("E2E Unhappy Path Journey - Missing subject field", async () => {
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiIwNmZmN2QxOS1iN2Y1LTRiZjctYmJhNi00NjRjY2FlZTdiMWQiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTQ2MDk3N30.-DgJu1RIgFgDMvTq5m2ICvzXBEvm_ICMkiWg8C7daqee9wCYZ9oRIgELoRIphxTkTQxsggVuIIvP2lCbJumGJA");
-        expect(userInfoResponse.status).toBe(400);
-    });
-
-    it.skip("E2E Unhappy Path Journey - Missing client session id field", async () => {
-        const userInfoResponse = await userInfoPost("Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYzODA1Y2UyLWQzNjYtNDY3ZC1iYzhkLWMzMTc4MGFmNTQyYyJ9.eyJzdWIiOiI0OTgyNGNlYS0yNzk0LTQyMTUtYjMwYy1hMTNiMThmM2JlN2QiLCJhdWQiOiJodHRwczovL3Jldmlldy1iYXYuZGV2LmFjY291bnQuZ292LnVrIiwiaXNzIjoiaHR0cHM6Ly9yZXZpZXctYmF2LmRldi5hY2NvdW50Lmdvdi51ayIsImV4cCI6MTY5OTUzNDQ1NH0.UXhKNSAEGkWuFKRaF_3ZFn5RtPnkv3XftW5Cn6Xci1HtjhALPpXDzSPgdouYNm8ExSNMouBOKJ3PjbgPVsFxpw");
-        expect(userInfoResponse.status).toBe(400);
-    });
 });
 
 describe("E2E Unhappy Path /authorisation Endpoint", () => {
@@ -82,7 +72,7 @@ describe("E2E Unhappy Path /authorisation Endpoint", () => {
 		sessionId = await startStubServiceAndReturnSessionId(bavStubPayload);
 	});
 
-	it.skip("Incorrect session state", async () => {
+	it("Incorrect session state", async () => {
 		const authResponse = await authorizationGet(sessionId);
 		expect(authResponse.status).toBe(401);
 	});
