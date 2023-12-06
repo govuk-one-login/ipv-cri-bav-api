94342d3f8e08afa4932bfab19036d364439645da
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-12-06 16:32:33 +0000

    KIWI-1504: Adds lambda that returns name

---
+++ src/services/PersonInfoRequestProcessor.ts
@@ -0,0 +1,67 @@
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { BavService } from "./BavService";
+import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
+import { MessageCodes } from "../models/enums/MessageCodes";
+import { EnvironmentVariables } from "../utils/Constants";
+import { createDynamoDbClient } from "../utils/DynamoDBFactory";
+import { checkEnvironmentVariable } from "../utils/EnvironmentVariables";
+import { getFullName } from "../utils/PersonIdentityUtils";
+import { Response } from "../utils/Response";
+
+export class PersonInfoRequestProcessor {
+  private static instance: PersonInfoRequestProcessor;
+
+  private readonly logger: Logger;
+
+  private readonly metrics: Metrics;
+
+  private readonly BavService: BavService;
+
+	private readonly personIdentityTableName: string;
+
+	constructor(logger: Logger, metrics: Metrics) {
+		this.personIdentityTableName = checkEnvironmentVariable(EnvironmentVariables.PERSON_IDENTITY_TABLE_NAME, logger);
+  	const sessionTableName: string = checkEnvironmentVariable(EnvironmentVariables.SESSION_TABLE, logger);
+
+  	this.logger = logger;
+  	this.metrics = metrics;
+  	this.BavService = BavService.getInstance(sessionTableName, this.logger, createDynamoDbClient());
+	}
+
+	static getInstance(logger: Logger, metrics: Metrics): PersonInfoRequestProcessor {
+  	if (!PersonInfoRequestProcessor.instance) {
+  		PersonInfoRequestProcessor.instance = new PersonInfoRequestProcessor(logger, metrics);
+  	}
+  	return PersonInfoRequestProcessor.instance;
+	}
+
+	async processRequest(sessionId: string): Promise<Response> {
+  	const person = await this.BavService.getPersonIdentityById(sessionId, this.personIdentityTableName);
+  	const session = await this.BavService.getSessionById(sessionId);
+
+		if (!person) {
+  		this.logger.error("No person found for session id", {
+  			messageCode: MessageCodes.PERSON_NOT_FOUND,
+  		});
+  		return new Response(HttpCodesEnum.UNAUTHORIZED, `No person found with the session id: ${sessionId}`);
+  	}
+
+  	if (!session) {
+  		this.logger.error("No session found for session id", {
+  			messageCode: MessageCodes.SESSION_NOT_FOUND,
+  		});
+  		return new Response(HttpCodesEnum.UNAUTHORIZED, `No session found with the session id: ${sessionId}`);
+  	}
+
+  	this.logger.appendKeys({
+  		govuk_signin_journey_id: session?.clientSessionId,
+  	});
+
+  	const name = getFullName(person.name);
+
+		console.log("name", name);
+
+  	return new Response(HttpCodesEnum.OK, `name is ${name}`);
+	}
+}
