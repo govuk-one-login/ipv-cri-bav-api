7b1b0253fa8b4ed490f7b73ca83eac28294b1159
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-12-12 10:04:07 +0000

    [KIWI-1412] - Save and view partial name matches in Athena

--- src/models/IHmrcResponse.ts
+++ src/models/IHmrcResponse.ts
@@ -16,3 +16,24 @@ export interface HmrcVerifyResponse {
 	nameMatches: string;
 	accountName: string;
 }
+
+export interface PartialNameExport {
+	itemNumber: string;
+	timeStamp: number;
+	cicName: string;
+	accountName: string;
+	accountExists: string;
+	nameMatches: string;
+}
+
+// itemNumber: index (generated UUID)
+//
+// timeStamp: Timestamp when the response was received
+//
+// cicName: Name as received from CIC
+//
+// accountName: Name as received from HMRC bank account verification API
+//
+// accountExists: The value as received from HMRC bank account verification API
+//
+// nameMatches: The value as received from HMRC bank account verification API
