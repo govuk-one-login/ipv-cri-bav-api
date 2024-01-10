73c15884720d020fc3afa8a3bb5cf0f571e8596b
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-01-10 11:23:33 +0000

    KIWI-1523: remove log

--- src/services/UserInfoRequestProcessor.ts
+++ src/services/UserInfoRequestProcessor.ts
@@ -175,33 +175,6 @@ export class UserInfoRequestProcessor {
 				 },
 				});
 
-			console.log("hi", {
-				event_name: TxmaEventNames.BAV_CRI_VC_ISSUED,
-				...txmaCoreFields,
-				restricted:{
-					name: personInfo.name,
-					bankAccount: [{
-						sortCode: personInfo.sortCode,
-						accountNumber: personInfo.accountNumber,
-					}],
-				  },
-				extensions: {
-					evidence: [
-						{
-							txn: session.hmrcUuid!,
-							strengthScore: evidenceInfo.strengthScore,
-							validityScore: evidenceInfo.validityScore,
-							attemptNum: session.retryCount || 1,
-							ci: evidenceInfo.ci,
-							ciReasons: [{
-								ci: evidenceInfo.ci?.[0],
-								reason: session.copCheckResult,
-							}],
-						},
-					],
-				 },
-			});
-
 			await this.BavService.sendToTXMA(
 				this.txmaQueueUrl, {
 					event_name: TxmaEventNames.BAV_CRI_END,
