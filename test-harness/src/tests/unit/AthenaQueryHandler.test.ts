902f9034b7d1bac032e0a2eaa0dc12b166bbb4a7
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2024-04-02 19:40:59 +0100

    additional unit test

--- test-harness/src/tests/unit/AthenaQueryHandler.test.ts
+++ test-harness/src/tests/unit/AthenaQueryHandler.test.ts
@@ -1,4 +1,4 @@
-import {lambdaHandler, logger} from "../../AthenaQueryHandler";
+import {lambdaHandler} from "../../AthenaQueryHandler";
 import {APIGatewayProxyEvent} from "aws-lambda";
 import {mockClient} from "aws-sdk-client-mock";
 import {
@@ -34,6 +34,27 @@ describe("AthenaQueryHandler", () => {
     queryStringParameters: {
       "min-timestamp": "1234",
       "name-prefix": "ABCD",
+    },
+  };
+
+  const athenaGetQueryResultsMockResponse = {
+    ResultSet: {
+      Rows: [
+        {
+          Data: [
+            {
+              VarCharValue: "itemnumber",
+            },
+          ],
+        },
+        {
+          Data: [
+            {
+              VarCharValue: "12345678-1234-1234-1234-123456789012",
+            },
+          ],
+        },
+      ],
     },
   };
 
@@ -118,27 +139,7 @@ describe("AthenaQueryHandler", () => {
         },
       },
     });
-    // [{"Data":[{"VarCharValue":"itemnumber"}]},{"Data":[{"VarCharValue":"2853f0c6-082b-4020-8d60-7f713d7a3baa"}]}]
-    athenaMock.on(GetQueryResultsCommand).resolves({
-      ResultSet: {
-        Rows: [
-          {
-            Data: [
-              {
-                VarCharValue: "itemnumber",
-              }
-            ]
-          },
-          {
-            Data: [
-              {
-                VarCharValue: "12345678-1234-1234-1234-123456789012",
-              }
-            ]
-          }
-        ],
-      },
-    });
+    athenaMock.on(GetQueryResultsCommand).resolves(athenaGetQueryResultsMockResponse);
 
     // ACT
     const result = await lambdaHandler(lambdaProxyEvent as unknown as APIGatewayProxyEvent, context);
@@ -150,9 +151,50 @@ describe("AthenaQueryHandler", () => {
         }
     ]);
     expect(result.statusCode).toEqual(200);
+
   })
 
-  // it("should wait until the query has finished running")
+  it("should wait until the query has finished running", async () => {
+
+    // ARRANGE
+    athenaMock.on(StartQueryExecutionCommand).resolves({
+      QueryExecutionId: "1234",
+    })
+    athenaMock
+      .on(GetQueryExecutionCommand)
+      .resolvesOnce({
+        QueryExecution: {
+          Status: {
+            State: QueryExecutionState.QUEUED,
+          },
+        },
+      })
+      .resolvesOnce({
+        QueryExecution: {
+          Status: {
+            State: QueryExecutionState.RUNNING,
+          },
+        },
+      })
+      .resolves({
+        QueryExecution: {
+          Status: {
+            State: QueryExecutionState.SUCCEEDED,
+          },
+        },
+      });
+    athenaMock.on(GetQueryResultsCommand).resolves(athenaGetQueryResultsMockResponse);
+
+    // ACT
+    const result = await lambdaHandler(lambdaProxyEvent as unknown as APIGatewayProxyEvent, context);
+
+    // ASSERT
+    expect(result.statusCode).toEqual(200);
+    expect(athenaMock).toHaveReceivedCommandTimes(GetQueryExecutionCommand, 3);
+  });
+
+
+
   // it("should return 500 if the query is cancelled")
   // it("should return 500 if the query failed")
   // it("should return 500 if an unknown query state is encountered")
