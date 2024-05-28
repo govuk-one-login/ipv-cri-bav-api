d79543dc38d9125f6bc12aa0b17ae3f14a28db1d
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-05-28 14:16:45 +0100

    Linting

--- src/services/VerifyAccountRequestProcessor.ts
+++ src/services/VerifyAccountRequestProcessor.ts
@@ -181,7 +181,7 @@ export class VerifyAccountRequestProcessor {
 				accountName: verifyResponse.accountName,
 				accountExists: verifyResponse.accountExists,
 				nameMatches: verifyResponse.nameMatches,
-				sortCodeBankName: verifyResponse.sortCodeBankName
+				sortCodeBankName: verifyResponse.sortCodeBankName,
 			};
 			
 			await this.BavService.savePartialNameInfo(this.partialNameQueueUrl, partialNameRecord);
