cc5f07e1643a417c5b22529a1258c43a21654b6c
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-09-11 22:52:04 +0100

    Reinstated real contract test

--- src/tests/contract/BavCriProvider.test.ts
+++ src/tests/contract/BavCriProvider.test.ts
@@ -1,7 +1,6 @@
 import { Logger } from "@aws-lambda-powertools/logger";
 import { Verifier, VerifierOptions } from "@pact-foundation/pact";
 import { Constants } from "./utils/Constants";
-import path from "path";
 
 const logger = new Logger({
 	logLevel: "INFO",
@@ -9,7 +8,6 @@ const logger = new Logger({
 });
 
 let opts: VerifierOptions;
-const pactFile = path.resolve("./tests/contract/data/IpvCoreBack-BavCriProvider.json");
 // Verify that the provider meets all consumer expectations
 describe("Pact Verification", () => {
 	beforeAll(() => {  
@@ -18,17 +16,16 @@ describe("Pact Verification", () => {
 			provider: "BavCriProvider",
 			// we are starting the provider locally
 			providerBaseUrl: `${Constants.LOCAL_HOST}:${Constants.LOCAL_APP_PORT}`,
-			pactUrls: [pactFile],
-			// pactBrokerUrl: process.env.PACT_BROKER_URL,
-			// pactBrokerUsername: process.env.PACT_BROKER_USER,
-			// pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
-			// consumerVersionSelectors: [
-			//  { mainBranch: true },
-			//  { deployedOrReleased: true },
-			//   ],         
-			publishVerificationResult: false,
+			pactBrokerUrl: process.env.PACT_BROKER_URL,
+			pactBrokerUsername: process.env.PACT_BROKER_USER,
+    		pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+			consumerVersionSelectors: [
+				{ mainBranch: true },
+				{ deployedOrReleased: true },
+			  ],			
+			publishVerificationResult: true,
 			providerVersion: process.env.PACT_PROVIDER_VERSION,
-			logLevel: "debug",
+			logLevel: "info",
 		};
 	});  
   
