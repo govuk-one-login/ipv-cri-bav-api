9db4560731f492d08602a796775c71229699c87d
ZacMohamoud01 <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-11-15 14:23:33 +0000

    Resolve conflicts

--- src/tests/api/ApiUtils.ts
+++ src/tests/api/ApiUtils.ts
@@ -138,7 +138,7 @@ function deepEqual(a: any, b: any): boolean {
 }
 
 export function validateTxMAEventField(
-	{ eventName, jsonPath, expectedValue }: { eventName: TxmaEventName; jsonPath: string; expectedValue: string | number | string[] | number[] }, 
+	{ eventName, jsonPath, expectedValue }: { eventName: TxmaEventName; jsonPath: string; expectedValue: any },
 	allTxmaEventBodies: AllTxmaEvents = {},
 ): void {
 	const currentEventBody: TxmaEvent | undefined = allTxmaEventBodies[eventName];
@@ -149,15 +149,6 @@ export function validateTxMAEventField(
 
 			if (!deepEqual(expectedValue, actualValue)) {
 				throw new Error(`Validation failed: Expected ${JSON.stringify(expectedValue)} but found ${JSON.stringify(actualValue)} for key path "${jsonPath}" in event ${eventName}`);
-			if (Array.isArray(expectedValue)) {
-				if (!Array.isArray(actualValue) || actualValue.length !== expectedValue.length || 
-					!actualValue.every((val, index) => val === expectedValue[index])) {
-					throw new Error(`Validation failed: Expected array ${JSON.stringify(expectedValue)} but found ${JSON.stringify(actualValue)} for key path "${jsonPath}" in event ${eventName}`);
-				}
-			} else {
-				if (actualValue !== expectedValue) {
-					throw new Error(`Validation failed: Expected ${expectedValue} but found ${actualValue} for key path "${jsonPath}" in event ${eventName}`);
-				}
 			}
 		} catch (error) {
 			console.error(`Error validating key path "${jsonPath}" in event ${eventName}`, error);
