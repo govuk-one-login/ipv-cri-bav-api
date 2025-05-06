62b3c71ba81df74f299e4dad6b9cbfc2e92d9fb4
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-05-06 08:20:59 +0100

    Initial setup

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -45,6 +45,12 @@ export class Constants {
 	static readonly GRANT_TYPE = "grant_type";
 
 	static readonly AUTHORIZATION_CODE = "authorization_code";
+	
+	static readonly CLIENT_ASSERTION = "client_assertion";
+
+	static readonly CLIENT_ASSERTION_TYPE = "client_assertion_type";
+
+	static readonly CLIENT_ASSERTION_TYPE_JWT_BEARER = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
 
 	static readonly TOKEN_EXPIRY_SECONDS = 3600;
 
