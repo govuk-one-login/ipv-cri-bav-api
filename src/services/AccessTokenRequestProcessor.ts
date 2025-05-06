96e68301124f2b496d4abe1958f2ed821aaf4bfc
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-05-06 12:51:11 +0100

    Added check for KID in /token

--- src/services/AccessTokenRequestProcessor.ts
+++ src/services/AccessTokenRequestProcessor.ts
@@ -13,6 +13,12 @@ import { checkEnvironmentVariable } from "../utils/EnvironmentVariables";
 import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
 import { isPayloadValid, validateTokenRequestToRecord } from "../utils/AccessTokenRequestValidations";
 import { AuthSessionState } from "../models/enums/AuthSessionState";
+import { Jwt } from "../models/IVeriCredential";
+
+interface ClientConfig {
+	jwksEndpoint: string;
+	clientId: string;
+}
 
 export class AccessTokenRequestProcessor {
 	private static instance: AccessTokenRequestProcessor;
@@ -29,6 +35,8 @@ export class AccessTokenRequestProcessor {
 
 	private readonly dnsSuffix: string;
 
+	private readonly clientConfig: string;
+
 	constructor(logger: Logger, metrics: Metrics) {
 		this.logger = logger;
 		this.metrics = metrics;
@@ -39,6 +47,7 @@ export class AccessTokenRequestProcessor {
   		const signingKeyArn: string = checkEnvironmentVariable(EnvironmentVariables.KMS_KEY_ARN, this.logger);
 		this.issuer = checkEnvironmentVariable(EnvironmentVariables.ISSUER, this.logger);
 		this.dnsSuffix = checkEnvironmentVariable(EnvironmentVariables.DNSSUFFIX, this.logger);
+		this.clientConfig = checkEnvironmentVariable(EnvironmentVariables.CLIENT_CONFIG, this.logger);
 		
 		this.kmsJwtAdapter = new KmsJwtAdapter(signingKeyArn, this.logger);
 		this.bavService = BavService.getInstance(sessionTableName, this.logger, createDynamoDbClient());
@@ -75,7 +84,68 @@ export class AccessTokenRequestProcessor {
 				govuk_signin_journey_id: session?.clientSessionId,
 			});
 
+			let configClient: ClientConfig | undefined;
+			try {
+				const config = JSON.parse(this.clientConfig) as ClientConfig[];
+				configClient = config.find(c => c.clientId === session.clientId);
+			} catch (error: any) {
+				this.logger.error("Invalid or missing client configuration table", {
+					error,
+					messageCode: MessageCodes.MISSING_CONFIGURATION,
+				});
+				return Response(HttpCodesEnum.SERVER_ERROR, "Server Error");
+			}
+
+			console.log("CLIENTS", this.clientConfig)
+
+			if (!configClient) {
+				this.logger.error("Unrecognised client in request", {
+					messageCode: MessageCodes.UNRECOGNISED_CLIENT,
+				});
+				return Response(HttpCodesEnum.BAD_REQUEST, "Bad Request");
+			}
+
 			if (session.authSessionState === AuthSessionState.BAV_AUTH_CODE_ISSUED) {
+
+				const jwt: string = requestPayload.client_assertion;
+				console.log("JWT", jwt);
+
+				let parsedJwt: Jwt;
+				try {
+					parsedJwt = this.kmsJwtAdapter.decode(jwt);
+				} catch (error: any) {
+					this.logger.error("Failed to decode supplied JWT", {
+						error,
+						messageCode: MessageCodes.FAILED_DECODING_JWT,
+					});
+					return Response(HttpCodesEnum.UNAUTHORIZED, "Unauthorized");
+				}
+
+				console.log("ParsedJWT", parsedJwt);
+
+				try {
+					if (configClient.jwksEndpoint) {
+						const payload = await this.kmsJwtAdapter.verifyWithJwks(jwt, configClient.jwksEndpoint, parsedJwt.header.kid);
+		  
+						if (!payload) {
+							this.logger.error("Failed to verify JWT", {
+								messageCode: MessageCodes.FAILED_VERIFYING_JWT,
+							});
+							return Response(HttpCodesEnum.UNAUTHORIZED, "Unauthorized");
+						}
+					} else {
+						this.logger.error("Incomplete Client Configuration", {
+							messageCode: MessageCodes.MISSING_CONFIGURATION,
+						});
+						return Response(HttpCodesEnum.SERVER_ERROR, "Server Error");
+					}
+				} catch (error: any) {
+					this.logger.error("Invalid request: Could not verify JWT", {
+						error,
+						messageCode: MessageCodes.FAILED_VERIFYING_JWT,
+					});
+					return Response(HttpCodesEnum.UNAUTHORIZED, "Unauthorized");
+				}
 
 				validateTokenRequestToRecord(session, requestPayload.redirectUri);
 				// Generate access token
