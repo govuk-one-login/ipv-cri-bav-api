7a4205f1c17d88ba37e57be7f6834283cad265a5
“Zac <zac.mohamoud@digital.cabinet-office.gov.uk>
2025-05-07 15:48:13 +0100

    Update run-tests.sh env variables

--- run-tests.sh
+++ run-tests.sh
@@ -11,7 +11,7 @@ declare error_code
 # The CFN variables seem to include quotes when used in tests these must be removed before assigning them.
 CFN_BAVBackendURL_NoQuotes=$(remove_quotes "$CFN_BAVBackendURL")
 export DEV_CRI_BAV_API_URL=$(echo ${CFN_BAVBackendURL_NoQuotes%/})
-export DEV_IPV_BAV_STUB_URL=$(remove_quotes $CFN_BAVIPVStubExecuteURL)/start
+export DEV_IPV_BAV_STUB_URL=$(remove_quotes $CFN_BAVIPVStubExecuteURL)
 export DEV_BAV_TEST_HARNESS_URL=$(remove_quotes "$CFN_BAVTestHarnessURL")
 export DEV_BAV_SESSION_TABLE_NAME=$(remove_quotes "$CFN_SessionTableName")
 export VC_SIGNING_KEY_ID=$(remove_quotes "$CFN_VcSigningKeyId")
