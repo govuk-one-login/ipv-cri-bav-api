4ee4a217297345b2bf63671ba046c8bd62e97188
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2025-05-29 12:40:40 +0100

    KIWI-2310: Aligning CRI shape and baselining dependencies

--- bav-ipv-stub/src/auth.types.ts
+++ bav-ipv-stub/src/auth.types.ts
@@ -1,5 +1,4 @@
-
-export interface EvidenceRequested{
+export interface EvidenceRequested {
   strengthScore?: number;
   validityScore?: number;
   verificationScore?: number;
@@ -8,50 +7,50 @@ export interface EvidenceRequested{
 }
 
 export interface JWTPayload {
-// Required as per OpenID Connect documentation  
-  iss: string
-  sub: string
-  aud: string
-  jti: string
-  exp: number
-// Additional optional values  
-  redirect_uri?: string
-  client_id?: string
-  response_type?: string
-  state?: string
-  nbf?: number
-  iat?: number
-  scope?: string
-  nonce?: string
-  evidence_requested?: EvidenceRequested | undefined
-  [key: string]: any
+  // Required as per OpenID Connect documentation
+  iss: string;
+  sub: string;
+  aud: string;
+  jti: string;
+  exp: number;
+  // Additional optional values
+  redirect_uri?: string;
+  client_id?: string;
+  response_type?: string;
+  state?: string;
+  nbf?: number;
+  iat?: number;
+  scope?: string;
+  nonce?: string;
+  evidence_requested?: EvidenceRequested | undefined;
+  [key: string]: unknown;
 }
 export interface Jwks {
-  keys: JsonWebKey[]
+  keys: JsonWebKey[];
 }
 export interface JsonWebKey {
-  alg: 'ES256' | 'RS256'
-  kid: string
-  kty: 'EC' | 'RSA'
-  use: 'sig' | 'enc'
-  crv?: string
-  d?: string
-  dp?: string
-  dq?: string
-  e?: string
-  ext?: boolean
-  k?: string
-  key_ops?: string[]
-  n?: string
-  oth?: RsaOtherPrimesInfo[]
-  p?: string
-  q?: string
-  qi?: string
-  x?: string
-  y?: string
+  alg: "ES256" | "RS256";
+  kid: string;
+  kty: "EC" | "RSA";
+  use: "sig" | "enc";
+  crv?: string;
+  d?: string;
+  dp?: string;
+  dq?: string;
+  e?: string;
+  ext?: boolean;
+  k?: string;
+  key_ops?: string[];
+  n?: string;
+  oth?: RsaOtherPrimesInfo[];
+  p?: string;
+  q?: string;
+  qi?: string;
+  x?: string;
+  y?: string;
 }
 export interface JwtHeader {
-  alg: 'ES256' | 'RS256'
-  typ?: string | undefined
-  kid?: string
+  alg: "ES256" | "RS256";
+  typ?: string | undefined;
+  kid?: string;
 }
--- test-harness/src/.eslintrc.js
+++ test-harness/src/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: ["eslint-plugin-tsdoc"],
   extends: [
     "plugin:@typescript-eslint/recommended", // recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
